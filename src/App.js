14b5e4f3dae5d6a7d5c035a4d594b44c11ff16bf
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-15 11:58:08 -0500

    Task 3: Create and render the Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Footer from './components/Footer';
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <CampsitesList />
+      <Footer />
     </div>
   );
 }
