ad15a66bc0b9f7e45fc139a710e70addd9a55ed0
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-24 05:30:20 -0500

    Fix conditional rendering in DisplayList to prevent rendering of undefined items

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -11,11 +11,11 @@ const DisplayList = () => {
   return (
     <Row>
       {items.map((item, idx) => {
-        return (
+        return(item && (
           <Col md className="m-1" key={idx}>
             <AnimatedDisplayCard item={item} />
           </Col>
-        );
+        ));
       })}
     </Row>
   );
