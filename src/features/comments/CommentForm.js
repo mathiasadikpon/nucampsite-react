4a3b5d755157bc6b71aa924a3969dd0039346cbf
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-01 11:49:57 -0500

    Render the form using Formik: Testing Task 2

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -24,7 +24,7 @@ const CommentForm = ({ campsiteId }) => {
       author: values.author,
     };
     console.log(comment);
-    setModalOpen (false);
+    setModalOpen(false);
   };
 
   return (
@@ -36,7 +36,48 @@ const CommentForm = ({ campsiteId }) => {
         <ModalHeader toggle={() => setModalOpen(false)}>
           Add Comment
         </ModalHeader>
-        <ModalBody>campsite: {campsiteId}</ModalBody>
+        <ModalBody>
+          <Formik
+            initialValues={{ rating: undefined, author: "", commentText: "" }}
+            onSubmit={handleSubmit}
+          >
+            <Form>
+              <FormGroup>
+                <Label htmlFor="rating">Rating</Label>
+                <Field name="rating" as="select" className="form-control">
+                  <option>Select...</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Field>
+              </FormGroup>
+
+              <FormGroup>
+                <Label htmlFor="author">Your Name</Label>
+                <Field
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label htmlFor="commentText">Comment</Label>
+                <Field
+                  name="commentText"
+                  as="textarea"
+                  rows="12"
+                  className="form-control"
+                />
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
       </Modal>
     </>
   );
