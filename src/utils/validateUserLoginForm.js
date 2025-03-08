9efc1a32a1d2a594a58d780c0657574882c287be
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-08 12:29:57 -0500

    Add validation utility for user login form inputs

---
+++ src/utils/validateUserLoginForm.js
@@ -0,0 +1,24 @@
+
+export const validateUserLoginForm = (values) => {
+    const error ={};
+    if (!values.username) {
+        error.username = "Required";
+    }
+    
+    else if(!values.username.length <6){
+        error.username = "Username must be at least 6 characters";
+    }
+    else if(!values.username.length >15){
+        error.username = "Username must be at most 15 characters";
+    }
+    if (!values.password) {
+        error.password = "Required";
+    }
+    else if(!values.password.length <8){
+        error.password = "Password must be at least 8 characters";
+    }
+    return error;
+
+        
+        
+};
