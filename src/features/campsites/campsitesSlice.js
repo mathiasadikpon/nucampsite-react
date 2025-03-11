087b609b15419bda56293b9b4a52bd8691ea1b55
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-10 23:14:53 -0400

    Create fetchCampsites() using createAsyncThunk()

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -3,8 +3,20 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { baseUrl } from "../../app/shared/baseUrl";
 import { mapImageURL } from "../../utils/mapImageURL";
 
+export const fetchCampsites = createAsyncThunk(
+  "campsites/fetchCampsites",
+  async () => {
+    const response = await fetch(baseUrl + "campsites");
+    if (!response.ok) {
+      return Promise.reject("Unable to fetch, status: " + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+);
+
 const initialState = {
-  campsitesArray: CAMPSITES,
+  campsitesArray: [],
 };
 
 const campsitesSlice = createSlice({
