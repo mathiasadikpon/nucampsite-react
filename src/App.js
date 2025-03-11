a14be622bcad341e765313f58e8ff3b2633eb387
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-11 00:44:23 -0400

    Fetch campsites on app initialization using useEffect

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+  
   return (
     <div className="App">
       <Header />
